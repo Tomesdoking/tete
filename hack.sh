cd5bb19a6fef449b4f3c6a5a2ef720fdfbac402b
tomesdoking <snaking80.super@gmail.com>
2016-12-23 12:04:00 -0800

    cleanup

--- hack.sh
+++ hack.sh
@@ -2,11 +2,11 @@ for Y in {2016..2016}
 do
   mkdir $Y
   cd $Y
-  for M in {06..06}
+  for M in {07..12}
   do
     mkdir $M
     cd $M
-    for D in {2..10}
+    for D in {2..23}
     do
       mkdir $D
       cd $D
